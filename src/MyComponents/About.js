bc8c6b5940af2c27afb3a4397b2115cc6ccc8303
Isha <ishasahu.2004@gmail.com>
2023-10-03 15:23:43 +0530

    final commit of files

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa est magni, quidem veritatis aspernatur quos quasi vel deserunt, similique eaque saepe recusandae quis perspiciatis enim quisquam provident rerum eum numquam?</p>
+        </div>
+    )
+}
